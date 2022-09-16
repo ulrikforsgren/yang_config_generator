4e0fcaf1ec1da02e96ec213f892227ff2fbd21b3
Ulrik Stridsman <uforsgre@cisco.com>
2022-09-16 09:16:39 +0200

    Code cleanup in xeger_minmax. Ignore token 'any'.

--- rstr/xeger.py
+++ rstr/xeger.py
@@ -112,8 +112,8 @@ class XegerMinMax(object):
         self._cases: Mapping[str, Callable[..., Any]] = {
             'literal': lambda x: (1, 1),
             'not_literal': lambda x: (1, 1),
-            'at': lambda x: [(0, 0)],  #???
-            'in': lambda x: self._handle_in(x),
+            'at': lambda x: (0, 0),  # Does this need to be ignored?
+            'in': lambda x: self._handle_in(x),  # Can this always return (1, 1)?
             'any': lambda x: (1, 1),
             'range': lambda x: (1, 1),
             'category': lambda x: (1, 1),
@@ -124,7 +124,7 @@ class XegerMinMax(object):
             'groupref': lambda x: self._cache[x],
             'min_repeat': lambda x: self._handle_repeat(*x),
             'max_repeat': lambda x: self._handle_repeat(*x),
-            'negate': lambda x: [False],
+            'negate': lambda x: False,
         }
 
     def xeger(self, string_or_regex: Union[str, Pattern[str]]) -> str:
@@ -134,7 +134,6 @@ class XegerMinMax(object):
             pattern = typing.cast(str, string_or_regex)
 
         parsed = sre_parse.parse(pattern)
-#        print(parsed)
         result = self._build_string(parsed)
         self._cache.clear()
         return result
@@ -146,39 +145,39 @@ class XegerMinMax(object):
         return functools.reduce(lambda a,b: (a[0]+b[0],a[1]+b[1]), newstr)
 
     def _handle_state(self, state: Any) -> Any:
-        #print(state)
         opcode, value = state
         opcode = opcode.name.lower()
         if opcode == 'category':
             value = value.name.lower()
-        return self._cases[opcode](value)
+        result = self._cases[opcode](value)
+        if opcode == 'in':
+            assert(result == (1, 1))
+        return result
 
     def _handle_group(self, value: Sequence[Any]) -> str:
         mmr = [self._handle_state(i) for i in value[-1]]
-#        mm = [functools.reduce(lambda a,b: (min(a[0],b[0]),max(a[1],b[1])), r) for r in mmr]
-#        print("group mmr", mmr)
         result = functools.reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]), mmr)
         if value[0]:
             self._cache[value[0]] = result
-#        print("group result", result)
         return result
 
     def _handle_in(self, value: Any) -> Any:
         candidates = [self._handle_state(i) for i in value]
+        if candidates[0] is False:  # Ignore negate
+            candidates = candidates[1:]
         return functools.reduce(lambda a,b: (min(a[0],b[0]),max(a[1],b[1])), candidates)
 
     def _handle_repeat(self, start_range: int, end_range: int, value: str) -> str:
         # TODO: Should it be possible to limit the output to STAR_PLUS_LIMIT
-#        print("Max repeat", start_range, end_range, value)
         mmr = [self._handle_state(i) for i in value]
-#        print("Max repeat mmr", mmr)
+        assert(len(mmr) == 1)
         result = functools.reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]), mmr)
 
         return start_range*result[0], end_range*result[1]
 
-    def _handle_branch(self, value: Any) -> Any: # Probably need to update this
+    def _handle_branch(self, value: Any) -> Any:  # Probably need to update this
         result = []
-        for b in value:
-            newstr = [self._handle_state(i) for i in b]
-            result.append(functools.reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]), newstr))
-        return functools.reduce(lambda a,b: (min(a[0],b[0]),max(a[1],b[1])), result)
+        for branch in value:
+            bresult = [self._handle_state(i) for i in branch]
+            result.append(functools.reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]), bresult))
+        return functools.reduce(lambda a, b: (min(a[0], b[0]), max(a[1], b[1])), result)
